26cc66959655adb0645800d3ebb1522d01d3356a
Kishore <kishore290499@gmail.com>
2024-01-15 11:51:33 +0530

    update docker lib files

--- src/main/java/com/example/studioVogue/StudioVogueApplication.java
+++ src/main/java/com/example/studioVogue/StudioVogueApplication.java
@@ -6,30 +6,29 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Objects;
+import java.io.InputStream;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.studioVogue")
 public class StudioVogueApplication {
 
-	public static void main(String[] args) throws IOException {
-		ClassLoader classLoader = StudioVogueApplication.class.getClassLoader();
-		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
-		FileInputStream serviceAccount =
-				new FileInputStream(file.getAbsoluteFile());
+    public static void main(String[] args) throws IOException {
+        InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
 
-		FirebaseOptions options = new FirebaseOptions.Builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.setDatabaseUrl("https://studio-vogue-sprinboot-default-rtdb.asia-southeast1.firebasedatabase.app")
-				.build();
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://studio-vogue-sprinboot-default-rtdb.asia-southeast1.firebasedatabase.app")
+                .build();
 
-		FirebaseApp.initializeApp(options);
+        FirebaseApp.initializeApp(options);
 
-		SpringApplication.run(StudioVogueApplication.class, args);
-	}
+        SpringApplication.run(StudioVogueApplication.class, args);
+    }
 
 }
